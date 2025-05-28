164e0ec90c6825669a87bb3daf4acbc9fbf63588
nicholaskotzias <nicokotzias@gmail.com>
2025-05-27 21:04:25 -0300

    feat: adicionar view da área administrativa com botões para gerenciar entidades do sistema
    - Links para níveis, eixos, cursos, turmas, alunos, categorias, comprovantes, documentos e declarações
    - Layout responsivo com botões grandes centralizados
    - Uso de rotas nomeadas para navegação

---
+++ resources/views/admin.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('title', 'Admin')
+
+@section('content')
+
+<div class="container mt-4">
+    <h1 class="mb-4 text-center">Área do ADM</h1>
+
+    <div class="d-flex flex-wrap justify-content-center gap-3">
+        <a href="{{ route('niveis.index') }}" class="btn btn-primary btn-lg px-4" style="min-width: 160px;">Nível</a>
+        <a href="{{ route('eixos.index') }}" class="btn btn-primary btn-lg px-4" style="min-width: 160px;">Eixo</a>
+        <a href="{{ route('cursos.index') }}" class="btn btn-primary btn-lg px-4" style="min-width: 160px;">Curso</a>
+        <a href="{{ route('turmas.index') }}" class="btn btn-primary btn-lg px-4" style="min-width: 160px;">Turma</a>
+        <a href="{{ route('alunos.index') }}" class="btn btn-primary btn-lg px-4" style="min-width: 160px;">Aluno</a>
+        <a href="{{ route('categorias.index') }}" class="btn btn-primary btn-lg px-4" style="min-width: 160px;">Categoria</a>
+        <a href="{{ route('comprovantes.index') }}" class="btn btn-primary btn-lg px-4" style="min-width: 160px;">Comprovante</a>
+        <a href="{{ route('admin.documentos') }}" class="btn btn-primary btn-lg px-4" style="min-width: 160px;">Documento</a>
+        <a href="{{ route('declaracoes.index') }}" class="btn btn-primary btn-lg px-4" style="min-width: 160px;">Declarações</a>
+    </div>
+</div>
+
+@endsection
