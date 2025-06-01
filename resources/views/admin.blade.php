0b48b16ac10de7ca379c9c72d5de16b10cdee224
nicholaskotzias <nicokotzias@gmail.com>
2025-06-01 00:50:52 -0300

    chore: alterar layout dos botões da área Admin para coluna e atualizar link Documentos
    - Trocar flex-wrap por flex-column para exibir os botões em coluna
    - Substituir link "Ir para documentos" por botão com rota admin.documentos
    - Ajustar texto e estilos para consistência dos botões

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -7,7 +7,7 @@
 <div class="container mt-4">
     <h1 class="mb-4 text-center">Área do ADM</h1>
 
-    <div class="d-flex flex-wrap justify-content-center gap-3">
+    <div class="d-flex flex-column justify-content-center gap-3">
         <a href="{{ route('niveis.index') }}" class="btn btn-primary btn-lg px-4" style="min-width: 160px;">Nível</a>
         <a href="{{ route('eixos.index') }}" class="btn btn-primary btn-lg px-4" style="min-width: 160px;">Eixo</a>
         <a href="{{ route('cursos.index') }}" class="btn btn-primary btn-lg px-4" style="min-width: 160px;">Curso</a>
@@ -15,8 +15,7 @@
         <a href="{{ route('alunos.index') }}" class="btn btn-primary btn-lg px-4" style="min-width: 160px;">Aluno</a>
         <a href="{{ route('categorias.index') }}" class="btn btn-primary btn-lg px-4" style="min-width: 160px;">Categoria</a>
         <a href="{{ route('comprovantes.index') }}" class="btn btn-primary btn-lg px-4" style="min-width: 160px;">Comprovante</a>
-        <a href="/admin/documentos">Ir para documentos</a>
-        <a href="{{ route('declaracoes.index') }}" class="btn btn-primary btn-lg px-4" style="min-width: 160px;">Declarações</a>
+        <a href="{{ route('admin.documentos') }}" class="btn btn-primary btn-lg px-4" style="min-width: 160px;">Documentos</a>
     </div>
 </div>
 
