c7a19aebfb60e13f9e6b3497280f5e7d53598a77
nicholaskotzias <nicokotzias@gmail.com>
2025-06-02 20:27:04 -0300

    feat: atualizar view área do aluno com mensagens de alerta e ajuste nos botões
    - Adicionadas exibições de alertas para mensagens de sucesso e erro

--- resources/views/aluno.blade.php
+++ resources/views/aluno.blade.php
@@ -7,9 +7,20 @@
 <div class="container mt-4">
     <h1 class="mb-4 text-center">Área do Aluno</h1>
 
+    @if (session('error'))
+        <x-alert tipo="danger">
+            {{ session('error') }}
+        </x-alert>
+    @endif
+
+    @if (session('success'))
+        <x-alert tipo="success" id="success-message">
+            {{ session('success') }}
+        </x-alert>
+    @endif
+
     <div class="d-flex flex-wrap justify-content-center gap-3">
-        <a href="{{ route('alunos.create') }}" class="btn btn-success btn-lg px-4" style="min-width: 200px;">Efetuar Cadastro</a>
-        <a href="{{ route('documentos.index') }}" class="btn btn-info btn-lg px-4" style="min-width: 200px;">Adicionar Documento</a>
+        <a href="{{ route('aluno.documentos.create') }}" class="btn btn-info btn-lg px-4" style="min-width: 200px;">Adicionar Documento</a>
     </div>
 </div>
 
