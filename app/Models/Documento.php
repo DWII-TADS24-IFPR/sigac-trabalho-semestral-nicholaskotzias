ce81e2cad61c39ac9e23b65f3daec7ba6b6f99d5
nicholaskotzias <nicokotzias@gmail.com>
2025-06-03 20:11:23 -0300

    feat: adicionar user_id ao fillable de documento, bem como a relação belongsTo

--- app/Models/Documento.php
+++ app/Models/Documento.php
@@ -7,9 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Documento extends Model
 {
     protected $table = 'documentos';
-    protected $fillable = ['url', 'descricao', 'horas_in', 'status', 'comentario', 'horas_out', 'categoria_id'];
+    protected $fillable = ['url', 'descricao', 'horas_in', 'status', 'comentario', 'horas_out', 'categoria_id', 'user_id'];
 
     public function categoria(){
         return $this->belongsTo(Categoria::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
     }
 }
